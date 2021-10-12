a93f3d7094917e0771862dbd0e99967adc91c08e
GitStudentSem <programmSem@yandex.ru>
2021-10-12 15:59:08 +0300

    minor fix

--- my-app/src/components/header/Header.jsx
+++ my-app/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import log_out from "../../icons/log_out.svg";
 const Header = (props) => {
   return (
     <header className={s.header}>
-      <h1 className={s.header_name}>Simple Hotel Check</h1>
+      <p className={s.header_name}>Simple Hotel Check</p>
       <button className={s.login_logout}>
         Выйти
         <img src={log_out} alt="Выйти из приложения" />
---
+++ my-app/src/components/login/Login.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Login = (props) => {
+  return <p className="">СТраница логина</p>;
+};
+export default Login;
